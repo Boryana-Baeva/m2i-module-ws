26853f188db9e82090950c043205cbb7c9a68855
Boryana Baeva <b.baeva@labom2iformation.fr>
2024-01-22 11:40:34 +0100

    add request POST/personnes

--- src/main/java/com/demo/api/personnes/PersonneApi.java
+++ src/main/java/com/demo/api/personnes/PersonneApi.java
@@ -1,8 +1,6 @@
 package com.demo.api.personnes;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 import java.util.ArrayList;
@@ -12,10 +10,16 @@ import java.util.List;
 public class PersonneApi {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Personne> getPersonnes() {
+    public List<Personne> getPersonnes(){
         ArrayList<Personne> resultat = new ArrayList<>();
         resultat.add(new Personne("JC", "Dominguez"));
         resultat.add(new Personne("Alain", "Dupont"));
         return resultat;
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void addPersonne(Personne newPersonne){
+        System.out.println(newPersonne);
     }
 }
