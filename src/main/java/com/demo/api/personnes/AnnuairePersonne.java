c202c3c2d1331a85a6608cc4ce7c2d54876fac5a
Boryana Baeva <b.baeva@labom2iformation.fr>
2024-01-22 12:05:06 +0100

    add AnnuairePersonne containing a map of Personne

---
+++ src/main/java/com/demo/api/personnes/AnnuairePersonne.java
@@ -0,0 +1,21 @@
+package com.demo.api.personnes;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class AnnuairePersonne {
+    private int idCount = 0;
+    private HashMap<Integer, Personne> personnes = new HashMap<>();
+
+    public void addPersonne(Personne newPersonne){
+        personnes.put(idCount++, newPersonne);
+    }
+
+    public List<Personne> getList(){
+        return personnes.values().stream().toList();
+    }
+
+    public Personne getPersonne(int id){
+        return personnes.get(id);
+    }
+}
--- src/main/java/com/demo/api/personnes/PersonneApi.java
+++ src/main/java/com/demo/api/personnes/PersonneApi.java
@@ -8,18 +8,21 @@ import java.util.List;
 
 @Path("/personnes")
 public class PersonneApi {
+    private static AnnuairePersonne annuaire = new AnnuairePersonne();
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Personne> getPersonnes(){
-        ArrayList<Personne> resultat = new ArrayList<>();
-        resultat.add(new Personne("JC", "Dominguez"));
-        resultat.add(new Personne("Alain", "Dupont"));
-        return resultat;
+//        ArrayList<Personne> resultat = new ArrayList<>();
+//        resultat.add(new Personne("JC", "Dominguez"));
+//        resultat.add(new Personne("Alain", "Dupont"));
+//        return resultat;
+        return annuaire.getList();
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public void addPersonne(Personne newPersonne){
         System.out.println(newPersonne);
+        annuaire.addPersonne(newPersonne);
     }
 }
